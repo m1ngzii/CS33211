5cf511920c7121d4f47bca4210b68287c99d4e1b
a thomas <arionnna.t@gmail.com>
2024-11-01 11:12:00 -0400

    Update consumer.cpp

--- Assignment1/consumer.cpp
+++ Assignment1/consumer.cpp
@@ -14,9 +14,9 @@
 int main(int argc, char *argv[]) {
 
     int shmShared = shm_open(shmPath, O_RDWR, S_IRUSR | S_IWUSR); 
-    if (shmShared == -1) { 
-        std::cerr << "Error handling memory in consumer, try again! Error code: " << strerror(errno) << std::endl; 
-        return 1; 
+    if(shmShared == -1) { 
+        std::cerr << "Waiting for shared memory to be created..." << std::endl;
+        sleep(1); // wait before retrying
     }
 
     ftruncate(shmShared, sizeof(int));
@@ -27,9 +27,10 @@ int main(int argc, char *argv[]) {
         close(shmShared);
         return 1; 
     }
-    // initializing mutexs 
+    // initializing semaphores, etc.  
     consumer->empty = sem_open("/empty_semaphore", 0); 
     consumer->full = sem_open("/full_semaphore", 0); 
+    consumer->out = 0; 
 
     if (consumer->empty == SEM_FAILED || consumer->full == SEM_FAILED) {
         std::cerr << "Error opening semaphores: " << strerror(errno) << std::endl;
@@ -40,6 +41,7 @@ int main(int argc, char *argv[]) {
 
 
     for (int i = 0; i < 5; ++i) {
+        sleep(1); 
         sem_wait(consumer->full); 
         sem_wait(consumer->mutex);  // locking crit. section 
 
